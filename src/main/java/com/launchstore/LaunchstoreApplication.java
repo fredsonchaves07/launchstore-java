ec766badf56462939fa955533e84824affcdad0e
Fredson Chaves <fredsonchaves07@gmail.com>
2020-09-23 13:38:21 -0300

    Organização do projeto

---
+++ src/main/java/com/launchstore/LaunchstoreApplication.java
@@ -0,0 +1,13 @@
+package com.launchstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaunchstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaunchstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/launchstore/LaunchstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.launchstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaunchstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
